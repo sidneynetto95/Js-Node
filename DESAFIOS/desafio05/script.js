bb72c9e5dac0531c4aeb3fc80445b38979558e39
Sidney da Costa Santos Neto <sidney.neto@dominio01.rj>
2022-04-28 16:21:49 -0300

    DESAFIO 5 INICIO

---
+++ DESAFIOS/desafio05/script.js
@@ -0,0 +1,8 @@
+var mt = window.document.getElementById('metros')
+var exe = window.document.getElementById('exec')
+exe.addEventListener('click',executar)
+
+function executar() {
+    var km = metros / 1000
+
+}
